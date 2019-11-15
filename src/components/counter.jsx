69ff240ab0bebdbdd1b9af35c74b1856865824b9
Nicholas Black <nicholas.c.black@leidos.com>
2019-11-14 23:38:55 -0500

    added getBadgeClasses method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,10 +10,16 @@ class Counter extends Component {
       <div>
         {/* this references the current object, state is an object with property count */}
         <img src={this.state.imageUrl} alt="" />
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
